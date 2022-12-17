3c8a27f236db7dd3f415d2fb85d830e441fbec1c
Jérôme FOURMOND <jerome.fourmond@lengow.com>
2022-12-17 23:37:10 +0100

    Update settings

--- piggy_bank/settings.py
+++ piggy_bank/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-38xxt8*lrg6&zso_!pu6a49a(1%afkuhrzwfgg1f6bt2r-=_*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
